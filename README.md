dd92aab485d56f969655bc9ec6c0c1d44f04b2eb
PbJBOSS <PbJBOSS@gmail.com>
2015-06-07 14:03:32 -0400

    Fixin stuff

--- README.md
+++ README.md
@@ -1,3 +1,47 @@
 # Bacon-Mod-1.6.4
 
 This is the src code for the Bacon Mod (http://www.minecraftforum.net/forums/mapping-and-modding/minecraft-mods/1286184-1-6-2-1-6-4-bacon-mod)
+
+#Changes from 1.6.4 to 1.7.10
+
+- Bacon.java
+ - NetworkRegistry.instance() -> NetworkRegistry.INSTANCE
+
+- BaconClient.java
+ - RegisterItemRenderer needs an item not a block, use Item.getItemFromBlock(static reference to block)
+ 
+- All gui classes
+ - fontRenderer -> fontRendererObj
+
+- RenderSloth.java
+ - don't know what function func_177_a is but its probably deobfuscated by now, I changed it to call super.doRender
+ 
+- All Block classes
+ - Icon is now IIcon
+ - MinecraftForge.setHarvestLevel is now a Block method: setHarvestLevel
+ - registerIIcons is now registerBlockIcons
+ - getBlockTileEntity is now getTileEntity
+ - getAABBPool().getAABB is now getBoundingBox
+ - getIIcon is now getIcon
+ - idPicked no longer exists
+ - getSubBlocks takes an Item instead of an int
+ - idDropped no longer exists
+ - Few methods take IBlockAccess instead of World object
+ - markTileEntityForDespawn no longer exists, just remove the block from the world.
+ - Lots of small changes is methods because of the change with ids
+ 
+- BaconItems.java
+ - EnumArmorMaterial -> ArmorMaterial
+ 
+- BaconCore.java
+ - GameRegistry.registerWorldGenerator now takes a class that implements IWorldGenerator and a gen weight
+ 
+- BaconWorld.java
+ - Change all id usage to static Block usage
+
+- BaconRecipes
+ - Static minecraft items moved from Item class to Items class, same with blocks (Block to Blocks)
+
+- Everywhere
+ - Ids are not use for blocks or items, use the static references to them
+ - @ForgeSubscribe -> @SubscribeEvent
---
+++ src/main/java/minecraftwero/baconMod/common/recipes/BaconRecipes.java
@@ -0,0 +1,176 @@
+package minecraftwero.baconMod.common.recipes;
+
+import minecraftwero.baconMod.common.config.BaconItemRegister;
+import minecraftwero.baconMod.common.config.BaconItems;
+import minecraftwero.baconMod.common.core.handlers.BaconMakerRecipes;
+import net.minecraft.init.Blocks;
+import net.minecraft.init.Items;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.oredict.OreDictionary;
+import cpw.mods.fml.common.registry.GameRegistry;
+
+public class BaconRecipes 
+{
+
+	public static void loadRecipes() 
+	{
+		if(BaconItems.baconHoe != null && BaconItems.cookedBacon != null)
+		{
+			GameRegistry.addRecipe(new ItemStack(BaconItems.baconHoe, 1),new Object[] { "BB", "S ", "S ", 'B', BaconItems.cookedBacon,'S', Items.stick });
+			GameRegistry.addRecipe(new ItemStack(BaconItems.baconHoe, 1),new Object[] { "BB", " S", " S", 'B', BaconItems.cookedBacon,'S', Items.stick });
+		}
+		
+		if(BaconItems.baconPickaxe != null && BaconItems.cookedBacon != null)
+		{
+			GameRegistry.addRecipe(new ItemStack(BaconItems.baconPickaxe, 1),new Object[] { "BBB", " S ", " S ", 'B', BaconItems.cookedBacon,'S', Items.stick });
+		}
+		
+		if(BaconItems.baconAxe != null && BaconItems.cookedBacon != null)
+		{
+			GameRegistry.addRecipe(new ItemStack(BaconItems.baconAxe, 1),new Object[] { "BB", "SB", "S ", 'B', BaconItems.cookedBacon,'S', Items.stick });
+			GameRegistry.addRecipe(new ItemStack(BaconItems.baconAxe, 1),new Object[] { "BB", "BS", " S", 'B', BaconItems.cookedBacon,'S', Items.stick });
+		}
+		
+		if(BaconItems.baconSpade != null && BaconItems.cookedBacon != null)
+		{
+			GameRegistry.addRecipe(new ItemStack(BaconItems.baconSpade, 1),new Object[] { "B", "S", "S", 'B', BaconItems.cookedBacon,'S', Items.stick });
+		}
+		
+		if(BaconItems.baconHelmet != null && BaconItems.cookedBacon != null)
+		{
+			GameRegistry.addRecipe(new ItemStack(BaconItems.baconHelmet, 1),new Object[] { "BBB", "B B", "   ", 'B',BaconItems.cookedBacon });
+		}
+		
+		if(BaconItems.baconPlate != null && BaconItems.cookedBacon != null)
+		{
+			GameRegistry.addRecipe(new ItemStack(BaconItems.baconPlate, 1), new Object[] {"B B", "BBB", "BBB", 'B', BaconItems.cookedBacon });
+		}
+		
+		if(BaconItems.baconLegs != null && BaconItems.cookedBacon != null)
+		{
+			GameRegistry.addRecipe(new ItemStack(BaconItems.baconLegs, 1), new Object[] {"BBB", "B B", "B B", 'B', BaconItems.cookedBacon });
+		}
+	
+		if(BaconItems.baconBoots != null && BaconItems.cookedBacon != null)
+		{
+			GameRegistry.addRecipe(new ItemStack(BaconItems.baconBoots, 1), new Object[] { "B B", "B B", 'B', BaconItems.cookedBacon });
+		}
+		
+		if(BaconItems.fineSword != null)
+		{		
+			GameRegistry.addRecipe(new ItemStack(BaconItems.fineSword,1), new Object [] {"CX", "CX", "C ", 'C', Items.stick, 'X', Items.iron_ingot});
+		}
+
+		if(BaconItems.baconSword != null && BaconItems.cookedBacon !=null)
+		{
+			GameRegistry.addRecipe(new ItemStack(BaconItems.baconSword, 1), new Object[] {"B", "B", "S", 'B', BaconItems.cookedBacon,'S', Items.stick });
+		}
+		
+		if(BaconItems.nutella != null)
+		{
+			GameRegistry.addRecipe(new ItemStack(BaconItems.nutella, 1), new Object[] {"SSS", "CCC", " M ", 'S', Items.sugar, 'C', new ItemStack(Items.dye, 1, 3), 'M', Items.milk_bucket});
+		}
+		
+		if(BaconItems.baconNutella != null && BaconItems.cookedBacon !=null && BaconItems.nutella != null)
+		{
+			GameRegistry.addRecipe(new ItemStack(BaconItems.baconNutella, 1), new Object[] {"B", "N", 'B', BaconItems.cookedBacon, 'N', new ItemStack(BaconItems.nutella, 1, OreDictionary.WILDCARD_VALUE)});
+		}
+		
+		if(BaconItems.sawMill != null)
+		{
+			GameRegistry.addRecipe(new ItemStack(BaconItems.sawMill, 1), new Object[] {"III", "IWI", "III", 'W', Items.iron_ingot, 'I', Blocks.iron_bars});
+		}
+		
+		if(BaconItems.baconMachine != null && BaconItemRegister.BaconMaker)
+		{
+			GameRegistry.addRecipe(new ItemStack(BaconItems.baconMachine, 1), new Object[]{"BDB", "C C", "YXY", 'D', Items.stick, 'B', Blocks.wooden_button, 'X', Blocks.furnace, 'Y', Blocks.stone, 'C', Blocks.piston});
+		}
+		
+		if(BaconItems.gayBacon != null && BaconItems.rawBacon !=null)
+		{
+			GameRegistry.addRecipe(new ItemStack(BaconItems.gayBacon, 1), new Object[]{" S ", "SRS", " S ", 'S', Items.sugar, 'R', BaconItems.rawBacon});
+		}
+		
+		if(BaconItems.baconSandwich != null && BaconItems.cookedBacon !=null)
+		{
+			GameRegistry.addRecipe(new ItemStack(BaconItems.baconSandwich, 1), new Object[]{" B ", " C ", " B ", 'B', Items.bread, 'C', BaconItems.cookedBacon});
+		}
+		
+		if(BaconItems.cheeseSlice != null && BaconItems.cheeseWheel !=null)
+		{
+			GameRegistry.addRecipe(new ItemStack(BaconItems.cheeseSlice, 1), new Object[]{" F ", " C ",  'C',new ItemStack(BaconItems.cheeseWheel, 1, OreDictionary.WILDCARD_VALUE), 'F', new ItemStack(BaconItems.fineSword, 1, OreDictionary.WILDCARD_VALUE)});
+			for(int i = 0; i<9;i++)
+			{
+				BaconMakerRecipes.getRecipes().addMillRecipe(new ItemStack(BaconItems.cheeseWheel, 1, i), new ItemStack(BaconItems.cheeseSlice, 9 - i));
+			}
+		}
+		
+		if(BaconItems.baconMachine != null && BaconItemRegister.RawBaconBlock)
+		{
+			GameRegistry.addRecipe(new ItemStack(BaconItems.baconMachine, 1, 1), new Object[]{"XXX", "XXX", "XXX", 'X', new ItemStack(BaconItems.rawBacon)});
+		}
+		
+		if(BaconItems.baconMachine != null && BaconItemRegister.CookedBaconBlock)
+		{
+			GameRegistry.addRecipe(new ItemStack(BaconItems.baconMachine, 1, 2), new Object[]{"XXX", "XXX", "XXX", 'X', new ItemStack(BaconItems.cookedBacon)});
+		}
+		if(BaconItemRegister.BaconLog && BaconItems.baconMachine != null)
+		{
+			OreDictionary.registerOre("logWood", new ItemStack(BaconItems.baconMachine, 1, 4));
+		}
+		
+		if(BaconItems.baconMachine != null && BaconItemRegister.BaconPlanks && BaconItemRegister.BaconLog)
+		{
+			GameRegistry.addRecipe(new ItemStack(BaconItems.baconMachine, 4, 7), new Object[]{"XXX", "XXX", "XXX", 'X', new ItemStack(BaconItems.baconMachine, 1, 4)});
+			OreDictionary.registerOre("plankWood", new ItemStack(BaconItems.baconMachine, 1, 7));
+		}
+		
+		if(BaconItems.baconDonut != null && BaconItems.cookedBacon != null && BaconItems.nutella != null && BaconItems.simpleDonut != null)
+		{
+			GameRegistry.addRecipe(new ItemStack(BaconItems.baconDonut, 1), new Object []{"BBB", " N ", " S ", 'B', BaconItems.cookedBacon, 'N', new ItemStack (BaconItems.nutella,1, OreDictionary.WILDCARD_VALUE), 'S', BaconItems.simpleDonut});
+		}
+		
+		if(BaconItems.baconSapling != null && BaconItems.cookedBacon !=null && BaconItems.rawBacon !=null && BaconItems.gayBacon !=null && BaconItems.magicBacon !=null)
+		{
+			GameRegistry.addRecipe(new ItemStack(BaconItems.baconSapling, 1), new Object[]{"RCR", "NSN", "MCM", 'R', BaconItems.rawBacon, 'C', BaconItems.cookedBacon, 'N', BaconItems.gayBacon, 'M', BaconItems.magicBacon, 'S', Blocks.sapling});
+		}
+		
+		if(BaconItems.simpleDonut != null)
+		{
+			GameRegistry.addRecipe(new ItemStack(BaconItems.simpleDonut, 2), new Object[]{"WWW", "WBW", "WWW", 'W', Items.wheat, 'B', Items.bread});
+		}
+		
+		if(BaconItems.rawBacon != null && BaconItems.cookedBacon != null)
+		{
+			GameRegistry.addSmelting(BaconItems.rawBacon, new ItemStack(BaconItems.cookedBacon), 0.1F);
+		}
+		
+		if(BaconItems.rawBacon != null)
+		{
+			BaconMakerRecipes.getRecipes().addMillRecipe(new ItemStack(Items.porkchop),  new ItemStack(BaconItems.rawBacon, 3));
+		}
+		
+		if(BaconItems.cheeseWheel != null)
+		{
+			GameRegistry.addSmelting(Items.milk_bucket, new ItemStack(BaconItems.cheeseWheel), 0.1F);
+		}
+		
+		if(BaconItems.baconMachine != null && BaconItemRegister.baconChest)
+		{
+			GameRegistry.addRecipe(new ItemStack(BaconItems.baconMachine, 1, 6), new Object[]{"XCX", "XVX", "XCX", 'X', Items.water_bucket, 'V', Blocks.chest, 'C', BaconItems.rawBacon});
+		}
+		
+		if(BaconItems.baconMachine != null && BaconItemRegister.BaconSandwichBlock)
+		{
+			GameRegistry.addRecipe(new ItemStack(BaconItems.baconMachine, 1, 3), new Object[]{" X ", "XYX", " X ", 'X', BaconItems.baconSandwich, 'Y', BaconItems.cookedBacon});
+		}
+		
+		
+
+
+		
+		
+	}
+
+	
+}
